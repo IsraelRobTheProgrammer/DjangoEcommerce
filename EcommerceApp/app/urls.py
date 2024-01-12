cc167cf3fe34fea92a1bad76a9150c5fabfc61c5
IsraelRobTheProgrammer <israelrobinson965@gmail.com>
2024-01-12 10:30:26 +0100

    Build:    (PN, Lassi),

--- EcommerceApp/app/urls.py
+++ EcommerceApp/app/urls.py
@@ -1,7 +1,13 @@
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 urlpatterns = [
     path("", views.home),
+    path("category/<slug:value>", views.category_view, name="category"),
+    path("category-title/<str:value>", views.category_title, name="category_title"),
+    path("product-detail/<int:id>", views.product_detail, name="product-detail"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- EcommerceApp/app/views.py
+++ EcommerceApp/app/views.py
@@ -1,7 +1,44 @@
-from django.shortcuts import render, HttpResponse
+from django.db.models import Count
+from django.shortcuts import render
+from django.views import View
+from .models import Product
 
 # Create your views here.
 
 
 def home(req):
     return render(req, "index.html")
+
+
+class CategoryView(View):
+    def get(self, req, value):
+        prod_obj = Product.objects.filter(category=value)
+        products = prod_obj.values("title")
+
+        return render(req, "category.html", locals())
+
+
+category_view = CategoryView.as_view()
+
+
+class CategoryTitle(View):
+    def get(self, req, value):
+        prod_obj = Product.objects.filter(title=value)
+        products = Product.objects.filter(category=prod_obj[0].category).values("title")
+
+        print(prod_obj, prod_obj[0])
+        return render(req, "category.html", locals())
+
+
+category_title = CategoryTitle.as_view()
+
+
+class ProductDetail(View):
+    def get(self, req, id):
+        product = Product.objects.get(id=id)
+        # print(product)
+
+        return render(req, "product_detail.html", locals())
+
+
+product_detail = ProductDetail.as_view()
